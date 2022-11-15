4d842ce4b2dbe67a20de6504f4ab7c6bce3ec90b
aannteet <117495581+aannteet@users.noreply.github.com>
2022-11-15 13:57:12 +0800

    Update bss.lua

--- bss.lua
+++ bss.lua
@@ -294,7 +294,6 @@ local buffTable = {
     ["Glue"] = {b = false, DecalID = "2504978518"},
     ["Glitter"] = {b = false, DecalID = "2542899798"},
     ["Tropical Drink"] = {b = false, DecalID = "3835877932"}
-    ["Stinger"] = {b = false, DecalID = "2314214749"}
 }
 local AccessoryTypes = require(game:GetService("ReplicatedStorage").Accessories).GetTypes()
 local MasksTable = {}
